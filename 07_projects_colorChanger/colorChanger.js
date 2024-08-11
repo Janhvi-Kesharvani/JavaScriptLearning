ce9c7b1209d756a230e7850a1cb25e15f58e30a5
Janhvi Kesharvani <janhvikesharvani@gmail.com>
2024-08-11 12:53:27 +0530

    Project_1 completed

--- 07_projects_colorChanger/colorChanger.js
+++ 07_projects_colorChanger/colorChanger.js
@@ -1,19 +1,58 @@
 const buttons = document.querySelectorAll(".button");
 // console.log(buttons);
 
-const body = document.querySelector.body;
+const body = document.querySelector("body");
 // buttons.forEach(function(button){
 //     console.log(button);
-
 // })
+
+/******** By using 'If else' */
 
 buttons.forEach((button) => {
   // console.log(button);
-  button.addEventListener("click", function (e) {
+  button.addEventListener('click', function (e) {
     console.log(e);
     console.log(e.target);
-    if (e.target.id === "grey") {
-        body.style.backgroundColor = e.target.id;
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'purple') {
+      body.style.backgroundColor = e.target.id;
     }
   });
 });
+
+// **************************** By using ''switch case***********************
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", function (e) {
+//     switch (e.target.id) {
+//       case "grey":
+//         body.style.backgroundColor = e.target.id;
+//         break;
+//       case "white":
+//         body.style.backgroundColor = e.target.id;
+//         break;
+//       case "blue":
+//         body.style.backgroundColor = e.target.id;
+//         break;
+//       case "yellow":
+//         body.style.backgroundColor = e.target.id;
+//         break;
+//       case "purple":
+//         body.style.backgroundColor = e.target.id;
+//         break;
+//       default:
+//         break;
+//     }
+//   });
+// });
